32529dc3cc74bbb10657a70bcb35d76c3059ae25
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-11 00:48:14 +0530

    feat : Welcome Message

---
+++ JSONInventoryDataManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JSONInventoryDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display Message
+            Console.WriteLine("Welcome to JSON Inventory Data Management");
+            Console.WriteLine("=========================================");
+        }
+    }
+}
