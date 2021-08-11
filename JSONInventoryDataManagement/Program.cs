d6eff8db78c86ddef3ff0cb3afa6044d6091d021
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-11 16:35:02 +0530

    feat: Add and Read Data From JSON file

--- JSONInventoryDataManagement/Program.cs
+++ JSONInventoryDataManagement/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace JSONInventoryDataManagement
 {
@@ -9,6 +14,9 @@ namespace JSONInventoryDataManagement
             //Display Message
             Console.WriteLine("Welcome to JSON Inventory Data Management");
             Console.WriteLine("=========================================");
+
+            //Calling Class InventoryData
+            InventoryData inventoryData = new InventoryData();            
         }
     }
 }
